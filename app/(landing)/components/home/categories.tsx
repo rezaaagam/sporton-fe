39f118816d8c7b8c1d1816eeb75ca7caa7568aca
rezaaagam <reza.agam13@gmail.com>
2026-01-18 11:42:55 +0700

    session 3.

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -1,51 +1,32 @@
+import { getImageUrl } from "@/app/lib/api";
+import { Category } from "@/app/types";
 import Image from "next/image";
 import Link from "next/link";
 import { FiArrowRight } from "react-icons/fi";
 
-const categoriesList = [
-    {
-        name: "Running",
-        imgUrl: "category-running.png",
-    },
-    {
-        name: "Tennis",
-        imgUrl: "category-tennis.png",
-    },
-    {
-        name: "Basketball",
-        imgUrl: "category-basketball.png",
-    },
-    {
-        name: "Football",
-        imgUrl: "category-football.png",
-    },
-    {
-        name: "Badminton",
-        imgUrl: "category-badminton.png",
-    },
-    {
-        name: "Swimming",
-        imgUrl: "category-swimming.png",
-    },
-];
+type TCategoriesProps = {
+  categories: Category[];
+};
 
-const CategoriesSection = () => {
+const CategoriesSection = ({ categories }: TCategoriesProps) => {
   return (
-    <section id="category-section" className="container mx-auto pb-20">
-        <div className="flex justify-between">
-            <h2 className="font-bold text-2xl">Browse By Categories</h2>
-            <Link href="#" className="flex gap-2 text-primary font-medium">
-                <span className="self-center">See All Categories</span>
-                <FiArrowRight className="self-center" />
-            </Link>
-        </div>
-        <div className="grid grid-cols-6 gap-12 mt-8">
-            {categoriesList.map((category, index) => (
-                <div
-                    className="rounded-lg bg-linear-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center" key={index}>
-                    <div className="self-center">
+    <section id="category-section" className="container mx-auto pb-20 mt-29">
+      <div className="flex justify-between">
+        <h2 className="font-bold text-2xl">Browse By Categories</h2>
+        <Link href="#" className="flex gap-2 text-primary font-medium">
+          <span className="self-center">See All Categories</span>
+          <FiArrowRight className="self-center" />
+        </Link>
+      </div>
+      <div className="grid grid-cols-6 gap-12 mt-8">
+        {categories.map((category) => (
+          <div
+            className="rounded-lg bg-linear-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center"
+            key={category._id}
+          >
+            <div className="self-center">
               <Image
-                src={`/images/categories/${category.imgUrl}`}
+                src={getImageUrl(category.imageUrl)}
                 width={86}
                 height="86"
                 alt={category.name}
@@ -61,4 +42,5 @@ const CategoriesSection = () => {
     </section>
   );
 };
+
 export default CategoriesSection;
--- app/(landing)/components/ui/cart-popup.tsx
+++ app/(landing)/components/ui/cart-popup.tsx
@@ -3,41 +3,16 @@ import Image from "next/image";
 import { FiArrowRight, FiTrash2 } from "react-icons/fi";
 import Button from "./button";
 import { useRouter } from "next/navigation";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { getImageUrl } from "@/app/lib/api";
 
-export const cartList = [
-  {
-    name: "SportsOn HyperFast Shoes",
-    category: "Running",
-    price: 100000,
-    qty: 2,
-    imgUrl: "product-3.png",
-  },
-  {
-    name: "SportsOn Rockets Tennis",
-    category: "Tennis",
-    price: 250000,
-    qty: 5,
-    imgUrl: "product-2.png",
-  },
-  {
-    name: "SportsOn Slowlivin",
-    category: "Running",
-    price: 200000,
-    qty: 3,
-    imgUrl: "product-1.png",
-  },
-    {
-    name: "SportsOn Slowlivin",
-    category: "Running",
-    price: 100000,
-    qty: 1,
-    imgUrl: "product-1.png",
-  },
-];
 
 const CartPopup = () => {
     const { push } = useRouter();
-    const totalPrice = cartList.reduce((total, item) => total + item.price * item.qty, 0);
+    const {items, removeItem} = useCartStore();
+
+    const totalPrice = items.reduce((total, item) => total + item.price * item.qty, 0);
+
 
     const handleCheckout = () => {
         push("/checkout")
@@ -48,10 +23,10 @@ const CartPopup = () => {
             <div className="p-4 border-b border-gray-200 font-bold text-center">
                 Shopping Cart
             </div>
-            {cartList.map((item, index) => (
+            {items.length ? items.map((item, index) => (
                     <div className="border-b border-gray-200 p-4 flex gap-3" key={index}>
                         <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
-                            <Image src={`/images/products/${item.imgUrl}`} width={63} height={63} alt={item.name} className="aspect-square object-contain"/>
+                            <Image src={getImageUrl(item.imageUrl)} width={63} height={63} alt={item.name} className="aspect-square object-contain"/>
                         </div>
                         <div className="self-center">
                             <div className="text-sm font-medium">{item.name}</div>
@@ -60,11 +35,13 @@ const CartPopup = () => {
                                 <div className="text-primary">{priceFormatter(item.price)}</div>
                             </div>
                         </div>
-                            <Button size="small" variant="ghost" className="w-7 h-7 p-0! self-center ml-auto">
+                            <Button size="small" variant="ghost" className="w-7 h-7 p-0! self-center ml-auto" onClick={() => removeItem(item._id)}>
                                 <FiTrash2 />
                             </Button>
                     </div>
-                ))}
+                )): (
+                    <div className="text-center opacity-50 py-5">Your shopping cart is Empty</div>
+                )}
                 <div className="border-t border-gray-200 p-4">
                     <div className="flex justify-between font-semibold">
                         <div className="text-sm">Total</div>
